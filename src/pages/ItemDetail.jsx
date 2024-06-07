0b79ef6f662518e9bc4666aba330b1371a145f69
CHOJaeGwan <jjgs0127@gmail.com>
2024-06-07 13:09:14 +0900

    server fix2

--- src/pages/ItemDetail.jsx
+++ src/pages/ItemDetail.jsx
@@ -94,7 +94,7 @@ function ItemDetail() {
     if (selectedSize && selectedDate && phone && name && address && item) {
       try {
         const response = await axios.post(
-          "https://ruden.loca.lt/api/send-email",
+          "https://lovely-turkey-67.loca.lt/api/send-email",
           {
             item: item,
             size: selectedSize,
